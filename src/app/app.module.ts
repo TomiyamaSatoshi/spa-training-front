eb2f16221acc385574235a11abd4b25d2fe47062
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-02-01 00:05:00 +0900

    add component article-list,header,footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
